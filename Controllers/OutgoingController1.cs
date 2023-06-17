e3580d12cb5eeda47c0e8b5a44c11e79214cd70a
BalikisBamgbose <balikisbamgbose@gmail.com>
2023-06-17 09:01:51 +0100

    StartWorkOnController

---
+++ Controllers/OutgoingController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Medics.Controllers
+{
+    public class OutgoingController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/RoleController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Medics.Controllers
+{
+    public class RoleController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
